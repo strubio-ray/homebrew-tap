4defc20efc6448c37fb226c7b787c2bc411cd87b
strubio-ray <105327421+strubio-ray@users.noreply.github.com>
2026-03-19 22:18:27 -0500

    vm-ward 1.8.6
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/vm-ward.rb
+++ Formula/vm-ward.rb
@@ -1,8 +1,8 @@
 class VmWard < Formula
   desc "Auto-halt daemon for forgotten Vagrant VMs"
   homepage "https://github.com/strubio-ray/vm-ward"
-  url "https://github.com/strubio-ray/vm-ward/archive/refs/tags/v1.8.5.tar.gz"
-  sha256 "b370a3920cfaa296570d2891c8fcc02cc805edc6704c1cbbc1827210c9a1bdcb"
+  url "https://github.com/strubio-ray/vm-ward/archive/refs/tags/v1.8.6.tar.gz"
+  sha256 "b4dd2da56e369d2c075c51a15eb2fd342c130a6fe3b84c045bf9ad9c935decd1"
   license "MIT"
 
   depends_on "go" => :build
