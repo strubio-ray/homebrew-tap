ac7a061713e08017452abf2ec6685b542474a614
Steven <105327421+strubio-ray@users.noreply.github.com>
2026-03-20 21:13:44 +0000

    fix: update mo SHA256s to CI-built binaries

--- Formula/mo.rb
+++ Formula/mo.rb
@@ -7,22 +7,22 @@ class Mo < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/strubio-ray/mo/releases/download/strubio-v0.1.0/mo_strubio-v0.1.0_darwin_arm64.zip"
-      sha256 "d0d1d368f038b7eefeff895505748d1e082b3202b6b6d14da7889f5997875b17"
+      sha256 "e395a7026464aeff76195ef4239c177c72757ba7ba3f5e589703ef1c52584ca1"
     end
     if Hardware::CPU.intel?
       url "https://github.com/strubio-ray/mo/releases/download/strubio-v0.1.0/mo_strubio-v0.1.0_darwin_amd64.zip"
-      sha256 "10fea8bb5bd46f4c4aed8f788075b32e7d75f1184692d22dc558bc445c7ea115"
+      sha256 "8093342c54b109ccb15c3abc8c69c0557d3870dc42223cfd1c6abb8d82818a50"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
       url "https://github.com/strubio-ray/mo/releases/download/strubio-v0.1.0/mo_strubio-v0.1.0_linux_arm64.tar.gz"
-      sha256 "a064724ebfc163ecfa1fa5e09ebe3b27eb3862381c0381914c91fafffd0c309b"
+      sha256 "bfb0d4af3e60516d44ada8c67491ae3bc53125d39f783a278eccef37cf14dcde"
     end
     if Hardware::CPU.intel?
       url "https://github.com/strubio-ray/mo/releases/download/strubio-v0.1.0/mo_strubio-v0.1.0_linux_amd64.tar.gz"
-      sha256 "4ae3e2891f2e714e733c3ecbff968385e4fa27b3cbe91fa9bea71bb360e528d3"
+      sha256 "f00a3783d6ef4e2f76e902b3b90649b4bbceee6b4e9fea0b88224295ade7ad7b"
     end
   end
 
