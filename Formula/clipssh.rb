42413d33080a4eb4a68191cc6a72037aa673ed92
Steven <105327421+strubio-ray@users.noreply.github.com>
2026-03-20 02:16:13 +0000

    feat: add clipssh-paste Swift compilation, remove pngpaste dependency

--- Formula/clipssh.rb
+++ Formula/clipssh.rb
@@ -1,19 +1,29 @@
 class Clipssh < Formula
-  desc "Send clipboard screenshots to remote SSH hosts"
+  desc "Send clipboard images to remote SSH hosts"
   homepage "https://github.com/strubio-ray/clipssh"
   url "https://github.com/strubio-ray/clipssh/archive/refs/tags/v1.1.2.tar.gz"
   sha256 "454583b42326053b14c3e5aed7a83fda7b0aade02e0b86d2370c50c6106c8ede"
   license "MIT"
 
   depends_on :macos
-  depends_on "pngpaste"
 
   def install
+    # Embed version in Swift source before compiling
+    inreplace "swift/ClipsshPaste.swift", 'let version = "1.0.0"', "let version = \"#{version}\""
+
+    # Compile Swift CLI helper (explicit AppKit link for robustness)
+    system "swiftc", "-O", "-framework", "AppKit", "-o", "clipssh-paste", "swift/ClipsshPaste.swift"
+
+    # Embed version in shell script
     inreplace "clipssh", "%%VERSION%%", version.to_s
+
+    # Install both binaries
     bin.install "clipssh"
+    bin.install "clipssh-paste"
   end
 
   test do
     assert_match "clipssh #{version}", shell_output("#{bin}/clipssh --version")
+    assert_match "clipssh-paste #{version}", shell_output("#{bin}/clipssh-paste --version")
   end
 end
