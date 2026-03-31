1751fd60a06cdc1daff68d3a04f8344928032968
strubio-ray <105327421+strubio-ray@users.noreply.github.com>
2026-03-30 21:28:49 -0500

    gmaps-sync 0.2.0
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/gmaps-sync.rb
+++ Formula/gmaps-sync.rb
@@ -1,8 +1,8 @@
 class GmapsSync < Formula
   desc "One-way sync from Google Maps saved places to local JSON"
   homepage "https://github.com/strubio-ray/gmaps-sync"
-  url "https://github.com/strubio-ray/gmaps-sync/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "3043dc56314c97fabf2bf4abbee5f63bf6f15f558eb2a590239185cecb3bccd8"
+  url "https://github.com/strubio-ray/gmaps-sync/archive/refs/tags/v0.2.0.tar.gz"
+  sha256 "ceee370b32186ce06de8be5e4b445df1cc1241e0b6317d9655329a8cb847a267"
   license "MIT"
 
   depends_on "node"
