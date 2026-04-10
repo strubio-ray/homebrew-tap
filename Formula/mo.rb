00cb7fc91819c8b36c51fdab644837a19532d04d
strubio-ray <105327421+strubio-ray@users.noreply.github.com>
2026-04-10 16:45:47 +0000

    feat: bump mo to strubio-v0.25.5

--- Formula/mo.rb
+++ Formula/mo.rb
@@ -1,28 +1,28 @@
 class Mo < Formula
   desc "Markdown viewer that opens .md files in a browser"
   homepage "https://github.com/strubio-ray/mo"
-  version "0.25.4"
+  version "0.25.5"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/strubio-ray/mo/releases/download/strubio-v0.25.4/mo_strubio-v0.25.4_darwin_arm64.zip"
-      sha256 "8a8bbb0acedbb1e49ea470b80cb303aa5231cdd6f87d68f191a2a3e2667140f5"
+      url "https://github.com/strubio-ray/mo/releases/download/strubio-v0.25.5/mo_strubio-v0.25.5_darwin_arm64.zip"
+      sha256 "ed9de5e043177f6d89e7f28b76faad4ff061fa58e94d039425492e0b45ece644"
     end
     if Hardware::CPU.intel?
-      url "https://github.com/strubio-ray/mo/releases/download/strubio-v0.25.4/mo_strubio-v0.25.4_darwin_amd64.zip"
-      sha256 "61e48f3cb466e36eca4650bb2870594f6cac3af47365bc3812506545b25bd2b8"
+      url "https://github.com/strubio-ray/mo/releases/download/strubio-v0.25.5/mo_strubio-v0.25.5_darwin_amd64.zip"
+      sha256 "7f79ca4eae49b9049836172146ee2e2f7a24b548592377dd5291facafca2458b"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/strubio-ray/mo/releases/download/strubio-v0.25.4/mo_strubio-v0.25.4_linux_arm64.tar.gz"
-      sha256 "8f61b3fcde4e57d63e6ec6e32e0d6049d8d43666dffff5ceb1b5d733b272042d"
+      url "https://github.com/strubio-ray/mo/releases/download/strubio-v0.25.5/mo_strubio-v0.25.5_linux_arm64.tar.gz"
+      sha256 "d96ceb925d9627ff53c3f1ac6a299065aa4fa42c16c0f2d19ffff176aa2ff7dd"
     end
     if Hardware::CPU.intel?
-      url "https://github.com/strubio-ray/mo/releases/download/strubio-v0.25.4/mo_strubio-v0.25.4_linux_amd64.tar.gz"
-      sha256 "1c023b67bd51ce197015fb51b3238bd809db67f0438689deb4a90983a3ada115"
+      url "https://github.com/strubio-ray/mo/releases/download/strubio-v0.25.5/mo_strubio-v0.25.5_linux_amd64.tar.gz"
+      sha256 "0861b2e49ef5798ea555830cddc03a75beed8ab50b2059cc6ead84e4dd8d1365"
     end
   end
 
